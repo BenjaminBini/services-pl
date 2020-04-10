e6eaa10d6e1cb38f3e0be6a400c977ec2781221b
Benjamin Bini <benjamin.bini@sully-group.fr>
2020-04-11 01:00:05 +0200

    Correction suppression CSS inutilisé

--- src/main/resources/postcss.config.js
+++ src/main/resources/postcss.config.js
@@ -6,15 +6,38 @@ const purgecss = require('@fullhuman/postcss-purgecss')({
     ],
 
     // Include any special characters you're using in this regular expression
-    defaultExtractor: content => content.match(/[\w-/.:]+(?<!:)/g) || require('purgecss-from-pug')(content) || []
+    defaultExtractor: content => purgeFromPug(content) || []
   })
 
   module.exports = {
     plugins: [
       require('tailwindcss'),
       require('autoprefixer'),
-      ...process.env.NODE_ENV == 'production'
-        ? [purgecss]
-        : []
+      purgecss
     ]
   }
+
+  
+const purgeFromPug = (content) => {
+    const tokens = require('pug-lexer')(content);
+    var selectors = [];
+    for (const token of tokens) {
+      switch (token.type) {
+        case "tag":
+        case "id":
+        case "class":
+          selectors.push(token.val);
+          break;
+        case "attribute":
+          if (token.name === "class" || token.name === "id") {
+            token.val = token.mustEscape ? token.val.replace(/"/g, "") : token.val
+            const classes = token.val.split(" ");
+            selectors = [...selectors, ...classes];
+          }
+          break;
+        default:
+          break;
+      }
+    }
+    return selectors;
+  };
